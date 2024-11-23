78211c29f93eb5cae89ccf7ed2501562869da16f
Hp <ahmedelzanaty001@gmail.com>
2024-11-23 19:28:22 +0200

    splash screen - home

--- app/src/main/java/com/example/muslimapp/SplashActivity.kt
+++ app/src/main/java/com/example/muslimapp/SplashActivity.kt
@@ -1,15 +1,23 @@
 package com.example.muslimapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+         android.os.Handler(Looper.getMainLooper()).postDelayed({
+             val intent = Intent(this, HomeActivity::class.java)
+             startActivity(intent)
+
+        }, 2000)
 
     }
 }
