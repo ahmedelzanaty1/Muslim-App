d21fb14741cf64df6152a1771e60526b629b215b
Hp <ahmedelzanaty001@gmail.com>
2024-11-26 13:52:01 +0200

    splash screen - home - Quran - Hadeth

--- app/src/main/java/com/example/muslimapp/fragments/hadethFragment.kt
+++ app/src/main/java/com/example/muslimapp/fragments/hadethFragment.kt
@@ -6,55 +6,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.muslimapp.R
+import com.example.muslimapp.adapters.DetailsAdapter
+import com.example.muslimapp.databinding.FragmentHadethBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [hadethFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class hadethFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentHadethBinding
+    lateinit var adapter : DetailsAdapter
+    lateinit var title : MutableList<String>
+    lateinit var desc : MutableList<String>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hadeth, container, false)
+        binding = FragmentHadethBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment hadethFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            hadethFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+
     }
+    fun init(view : View){
+        val allfile = requireActivity().assets.open("ahadeth.txt").bufferedReader().use {
+            it.readText()
+        }
+        val hadeth = allfile.trim().split("#")
+        title = mutableListOf()
+        desc = mutableListOf()
+        hadeth.forEach {hadeth ->
+            val hadethseperated = hadeth.trim().split("\n")
+            title.add(hadethseperated[0])
+            desc.add(hadeth)
+        }
+        adapter = DetailsAdapter(title)
+        binding.recyclerView.adapter = adapter
+
+
+    }
+
+
+
 }
