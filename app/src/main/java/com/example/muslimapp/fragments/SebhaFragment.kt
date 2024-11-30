62e02fe62bfbababd9b2955a486f4c501fd13e6d
Hp <ahmedelzanaty001@gmail.com>
2024-11-30 19:36:51 +0200

    sebha-fragment

--- app/src/main/java/com/example/muslimapp/fragments/SebhaFragment.kt
+++ app/src/main/java/com/example/muslimapp/fragments/SebhaFragment.kt
@@ -6,55 +6,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.muslimapp.R
+import com.example.muslimapp.databinding.FragmentSebhaBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SebhaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SebhaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding : FragmentSebhaBinding
+    var counter = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sebha, container, false)
+        binding = FragmentSebhaBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SebhaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SebhaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onsebhaclick()
     }
-}
+
+    private fun onsebhaclick() {
+        binding.sebhaTasbeh.text = "سبحان الله"
+        counter = 0
+        binding.sebhaTasbeh.setOnClickListener {
+            binding.sebhaNumber.text = counter.toString()
+            if (counter < 33) {
+                binding.bodySebha.rotation = (360 / 33 * counter).toFloat()
+                counter++
+                binding.sebhaNumber.text = counter.toString()
+
+                if (counter == 33) {
+                    counter = 0
+                    binding.sebhaTasbeh.text = "الحمد لله"
+                }
+            } else if (counter ==33) {
+                binding.bodySebha.rotation = (360 / 33 * (counter - 33)).toFloat()
+                counter++
+                binding.sebhaNumber.text = counter.toString()
+
+                if (counter == 33) {
+                    counter = 0
+                    binding.sebhaTasbeh.text = "الله أكبر"
+                }
+            } else if (counter ==33) {
+                binding.bodySebha.rotation = (360 / 33 * (counter - 66)).toFloat()
+                counter++
+                binding.sebhaNumber.text = counter.toString()
+
+                if (counter == 33) {
+                    counter = 0
+                    binding.sebhaTasbeh.text = "لا إلهَ إلاَّ اللَّه وحْدهُ لاَ شَرِيكَ لهُ"
+                }
+
+            }
+
+        }
+    }
+    }
+
+
+
